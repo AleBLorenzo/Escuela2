28a8d2a5fa671069e16c6065b7478b2da25dbb2f
Jorge Alejandro Brito Lorenzo <alejandrobl0819@gmail.com>
2025-05-01 13:47:51 +0000

    introducion de scanner

--- Copiar/src/CopiarFichero.java
+++ Copiar/src/CopiarFichero.java
@@ -3,25 +3,31 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
+import java.util.Scanner;
+
 
 public class CopiarFichero {
     public static void main(String[] args) throws IOException {
 
         File ficheroOrigen = new File("origen");
         ficheroOrigen.mkdirs();
-        File ficheroDestino = new File("destino");
-        ficheroDestino.mkdirs();
+       
 
-        File fichero = new File("origen//fichero.txt");
+        File fichero = new File(ficheroOrigen +"//fichero.txt");
 
         FileWriter fw = new FileWriter(fichero);
         BufferedWriter bw = new BufferedWriter(fw);
         bw.write("Esto es un fichero de texto");
         bw.close();
 
-        Files.copy(fichero.toPath(), new File("destino//fichero.txt").toPath());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el nombre de la Carpeta destino: ");
+        String nombreFichero = scanner.nextLine(); 
+
+        File ficheroDestino = new File(nombreFichero);
+        ficheroDestino.mkdirs();
+
+        Files.copy(fichero.toPath(), new File(ficheroDestino+"//fichero.txt").toPath());
 
         System.out.println("Fichero copiado correctamente");
 
