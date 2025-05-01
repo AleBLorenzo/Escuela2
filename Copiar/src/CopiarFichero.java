23f9f122d4a9720ea5e1bd3cea52b9aca5d95211
Jorge Alejandro Brito Lorenzo <alejandrobl0819@gmail.com>
2025-05-01 14:07:59 +0000

    se añadio el try-catch

--- Copiar/src/CopiarFichero.java
+++ Copiar/src/CopiarFichero.java
@@ -5,31 +5,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Scanner;
 
-
 public class CopiarFichero {
     public static void main(String[] args) throws IOException {
 
-        File ficheroOrigen = new File("origen");
-        ficheroOrigen.mkdirs();
-       
+        try {
+            File ficheroOrigen = new File("origen");
+            ficheroOrigen.mkdirs();
 
-        File fichero = new File(ficheroOrigen +"//fichero.txt");
+            File fichero = new File(ficheroOrigen + "//fichero.txt");
 
-        FileWriter fw = new FileWriter(fichero);
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write("Esto es un fichero de texto");
-        bw.close();
+            FileWriter fw = new FileWriter(fichero);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Esto es un fichero de texto");
+            bw.close();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce el nombre de la Carpeta destino: ");
-        String nombreFichero = scanner.nextLine(); 
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Introduce el nombre de la Carpeta destino: ");
+            String nombreFichero = scanner.nextLine();
 
-        File ficheroDestino = new File(nombreFichero);
-        ficheroDestino.mkdirs();
+            File ficheroDestino = new File(nombreFichero);
 
-        Files.copy(fichero.toPath(), new File(ficheroDestino+"//fichero.txt").toPath());
+            if (ficheroDestino.exists()) {
+                System.out.println("La carpeta ya existe");
+            } else {
+                System.out.println("La carpeta no existe se va a crear una nueva");
+                ficheroDestino.mkdirs();
 
-        System.out.println("Fichero copiado correctamente");
+            }
+
+            Files.copy(fichero.toPath(), new File(ficheroDestino + "//fichero.txt").toPath());
+
+            scanner.close();
+            System.out.println("Fichero copiado correctamente");
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
