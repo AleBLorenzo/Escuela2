d5f6f19ecb94656a5867c554b2b157be93362c0f
AleBLorenzo <alejandrobl0819@gmail.com>
2025-10-10 17:35:52 +0100

    Eje2 Json

---
+++ demo/src/main/java/com/example/Eje2.java
@@ -0,0 +1,104 @@
+package com.example;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Eje2 {
+
+    public static void main(String[] args) {
+
+        String[] generos = { "Drama", "Terror", "Romance" };
+        String[] generos2 = { "Drama", "Romance" };
+        String[] generos3 = { "Romance" };
+
+        File ruta = new File("src/main/java/com/example/datos.json");
+
+            JSONObject objetos = new JSONObject(new LinkedHashMap<>());
+
+            objetos.put("coleccion", "Mis Peliculas Favoritas");
+            objetos.put("fecha_creacion", LocalDate.now());
+            
+
+            JSONArray lista = new JSONArray();
+
+            JSONObject objetosl = new JSONObject(new LinkedHashMap<>());
+
+            objetosl.put("id", 1);
+            objetosl.put("titulo", "quijote");
+            objetosl.put("director", "Sancho");
+            objetosl.put("ano_lanzamiento", "1965");
+            objetosl.put("genero", generos);
+            objetosl.put("clasificacion", 8.2);
+
+            JSONObject premios = new JSONObject(new LinkedHashMap<>());
+
+            premios.put("oscar", true);
+            premios.put("glovo:oro", false);
+            premios.put("otros", 2);
+
+            objetosl.put("premios", premios);
+
+            lista.put(objetosl);
+
+            JSONObject objetos2 = new JSONObject(new LinkedHashMap<>());
+
+            objetos2.put("id", 2);
+            objetos2.put("titulo", "sssss");
+            objetos2.put("director", "Sanwwwcho");
+            objetos2.put("ano_lanzamiento", "1ee965");
+            objetos2.put("genero", generos2);
+            objetos2.put("clasificacion", 7.5);
+
+            JSONObject premios1 = new JSONObject(new LinkedHashMap<>());
+
+            premios1.put("oscar", true);
+            premios1.put("glovo:oro", false);
+            premios1.put("otros", 2);
+
+            objetos2.put("premios", premios1);
+
+            lista.put(objetos2);
+
+            JSONObject objetos3 = new JSONObject(new LinkedHashMap<>());
+
+            objetos3.put("id", 2);
+            objetos3.put("titulo", "sssss");
+            objetos3.put("director", "Sanwwwcho");
+            objetos3.put("ano_lanzamiento", "1ee965");
+            objetos3.put("genero", generos3);
+            objetos3.put("clasificacion", 7.5);
+
+            JSONObject premios2 = new JSONObject(new LinkedHashMap<>());
+
+            premios2.put("oscar", true);
+            premios2.put("glovo:oro", false);
+            premios2.put("otros", 2);
+
+            objetos3.put( "premios",premios2);
+
+            lista.put(objetos3);
+
+            objetos.put("peliculas", lista);
+            objetos.put("total_peliculas", lista.length());
+
+            try (BufferedWriter Json = new BufferedWriter(new FileWriter(ruta))) {
+                Json.write(objetos.toString(4));
+                System.out.println("Se creo 'Datos.json' correctamente");
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        System.out.println("Contenido");
+        System.out.println(objetos.toString(4));
+    }
+
+}
