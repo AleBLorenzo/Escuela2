f65c7606884178b21d7e0f7d7ab904c27babb9ec
AleBLorenzo <alejandrobl0819@gmail.com>
2025-10-31 09:02:31 +0000

    examen corrregido

--- examen/src/main/java/com/example/Main.java
+++ examen/src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,14 +11,20 @@ import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.DOMException;
-import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
 public class Main {
@@ -26,13 +33,13 @@ public class Main {
 
     private static ArrayList<Libro> datos = new ArrayList<>();
 
-    public static void main(String[] args) throws SAXException, IOException {
+    public static void main(String[] args) throws SAXException, IOException, DOMException, TransformerException {
 
-            leerLibrosBinario();
-            
-            escribirgson();
-           
-        xml(); 
+        leerLibrosBinario();
+
+        escribirgson();
+
+        xml();
 
     }
 
@@ -67,10 +74,20 @@ public class Main {
 
     private static void escribirgson() throws JsonSyntaxException {
 
-        Gson json = new Gson();
+        ArrayList<Libro> nuevo = new ArrayList<>();
+
+        Gson json = new GsonBuilder().setPrettyPrinting().create();
+
+        for (Libro libros : datos) {
+            if (libros.isDisponible()) {
+
+                nuevo.add(libros);
+            }
+
+        }
 
         try (BufferedWriter os = new BufferedWriter(new FileWriter("src/main/resources/libros_disponibles.json"))) {
-            String serializado = json.toJson(datos);
+            String serializado = json.toJson(nuevo);
             os.write(serializado);
 
         } catch (Exception e) {
@@ -78,19 +95,58 @@ public class Main {
         }
     }
 
-    private static void xml() throws DOMException {
+    private static void xml() throws DOMException, TransformerException {
+
         try {
-            
-            String datosd = datos.toString();
-            
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
-            DocumentBuilder build = factory.newDocumentBuilder();
-            DOMImplementation implementation = build.getDOMImplementation();
-            
-            Document docuemto = implementation.createDocument(null, "libros_disponibles", null);
-            
-            Element elemento = docuemto.createElement("datos");
-            
+            // Calcular estadísticas
+            int total = datos.size();
+            int disponibles = 0;
+            double sumaPrecios = 0.0;
+
+            for (Libro libro : datos) {
+                if (libro.isDisponible()) {
+                    disponibles++;
+                }
+                sumaPrecios += libro.getPrecio();
+            }
+
+            double precioMedio = sumaPrecios / total;
+
+            System.out.println("Estadísticas calculadas:");
+            System.out.println("Total libros: " + total);
+            System.out.println("libros disponibles: " + disponibles);
+            System.out.println("Precio medio: " + String.format("%.2f", precioMedio));
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            Element raiz = document.createElement("estadisticas");
+            document.appendChild(raiz);
+
+            Element elemTotalLibros = document.createElement("totalLibros");
+            elemTotalLibros.setTextContent(String.valueOf(total));
+            raiz.appendChild(elemTotalLibros);
+
+            Element elemdisponibles = document.createElement("disponibles");
+            elemdisponibles.setTextContent(String.valueOf(disponibles));
+            raiz.appendChild(elemdisponibles);
+
+            Element elemPrecioMedio = document.createElement("precioMedio");
+            elemPrecioMedio.setTextContent(String.format("%.2f", precioMedio));
+            raiz.appendChild(elemPrecioMedio);
+
+            TransformerFactory transFactory = TransformerFactory.newInstance();
+            Transformer transf = transFactory.newTransformer();
+
+            transf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File("src/main/resources/libros_datos.xml"));
+
+            transf.transform(source, result);
+
+            System.out.println("Archivo XML generado correctamente");
         } catch (ParserConfigurationException ex) {
         }
     }
