fe2cfc8db473494af7d3f1b37fdc4116eeb3e052
AleBLorenzo <alejandrobl0819@gmail.com>
2025-11-06 13:53:41 +0000

    algo de avance

---
+++ PracticaBinario/eje1analisis.md
@@ -0,0 +1,5 @@
+1 sacar  el tamaño del fichero con lo q se pueda hcaer y convertirlo a kb o mb 
+
+2- sacar el byte q mas se repite
+byte menos frecuente
+cantidad de bytes unicos diferentes
--- PracticaBinario/src/Eje1.java
+++ PracticaBinario/src/Eje1.java
@@ -3,41 +3,70 @@ import java.io.FileInputStream;
 
 public class Eje1 {
 
-
     public static void main(String[] args) throws Exception {
 
         final String RUTA = "ejercicio1_basico_ENUNCIADO.md";
 
         infoBasica(RUTA);
 
-
     }
 
     private static void infoBasica(String RUTA) {
-        //Creamos la entrada de datos con un FileInputStream especial para la
-        //la lectura de cualquier archivo
 
+        // Creamos la entrada de datos con un FileInputStream especial para la
+        // la lectura de cualquier archivo.
         try (FileInputStream rd = new FileInputStream(new File(RUTA))) {
 
-            //Con este bucle copiamos cada byte a una variable para almacenarlo
+            // Con este bucle copiamos cada byte a una variable para almacenarlo.
             int totalBytes = 0;
-            int bytes = 0;
+            int valortotal = 0;
 
-            while ((bytes = rd.read()) != -1) {
+            int[] bytes = new int[256];
 
-                totalBytes++;
+            while ((totalBytes = rd.read()) != -1) {
+
+                valortotal++;
+                bytes[totalBytes]++;
 
             }
 
-            //creamos un conversor para pasarlo a MB
+             int byteMasFrecuente = 0;
+        int frecuenciaMasAlta = bytes[0];
+        int byteMenosFrecuente = -1;
+        int frecuenciaMasBaja = Integer.MAX_VALUE;
+        int bytesUnicos = 0;
+        
+        for (int i = 0; i < 256; i++) {
+            if (bytes[i] > 0) {
+                bytesUnicos++;
+                
+                if (bytes[i] > frecuenciaMasAlta) {
+                    frecuenciaMasAlta = bytes[i];
+                    byteMasFrecuente = i;
+                }
+                
+                if (bytes[i] < frecuenciaMasBaja) {
+                    frecuenciaMasBaja = bytes[i];
+                    byteMenosFrecuente = i;
+                }
+            }
+        }
 
-            double convertidorMb = Math.round(totalBytes) / 1000.0;
 
-            //Imprimimos la info q nos da cada variable
+            // creamos un conversor para pasarlo a KB.
+            float convertidorKb = (float) (Math.round(valortotal) / 1024.0);
+            float convertidorMb = (float) (Math.round(convertidorKb) / 1024.0);
 
-            System.out.println("Total de Bytes des archivo :" + totalBytes + " bytes " + "(" + convertidorMb + " KB)");
+            System.out.println(byteMasFrecuente +"  "+frecuenciaMasAlta);
+            System.out.println(byteMenosFrecuente);
+            System.out.println(bytesUnicos);
+
+            // Imprimimos la info q nos da cada variable.
+            System.out.println("Total de Bytes des archivo :" + valortotal + " bytes " + "(" + convertidorKb + " KB)"
+                    + "(" + convertidorMb + " MB)");
 
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
