3a115720ab2f706cbb7e385b3950d49554ebaba4
AleBLorenzo <alejandrobl0819@gmail.com>
2025-11-17 16:39:18 +0000

    Listo

--- Repaso_ACD/RepasoPrimeraProgramacion/Ejer6/src/App.java
+++ Repaso_ACD/RepasoPrimeraProgramacion/Ejer6/src/App.java
@@ -1,13 +1,36 @@
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+
+    private static final Object Jugador = null;
+
     public static void main(String[] args) throws Exception {
+
+         ArrayList<Jugador> lista = new ArrayList();
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("Ejer6/src/Datos.dat"))) {
+            
+             
+                  Object obj;
+                while ((obj = in.readObject()) != null) {
+                        lista.add((Jugador) obj);
+                }
+
+        } catch (Exception e) {
+            System.out.println("Error "+e);
+        }
+
+       
      
         Scanner sc = new Scanner(System.in);
-        String nombre ;
+        String nombre = null ;
         String entrada = null ;
-        String entrada2 = null;
         int contadorpartida = -1;
         int racha = 0;
         int racha_Maxima = 0;
@@ -15,9 +38,7 @@ public class App {
         int derrotas = 0;
         int empates = 0;
 
-
-        System.out.println("===Bienvenido===\n");
-        System.out.print("Escriba su nombre de jugador: ");
+          System.out.print("Escriba su nombre de jugador: ");
         nombre = sc.nextLine();
 
         while (true) {
@@ -27,7 +48,7 @@ public class App {
 
             contadorpartida++;
 
-            if (entrada.equals("V")) {
+            if (entrada.toUpperCase().equals("V")) {
 
                 victorias++;
                 racha++;
@@ -40,38 +61,80 @@ public class App {
                     System.out.println("🔥 ¡"+racha+" VICTORIAS! ¡Imparable!");
                     
                 }
+
+                for (Jugador elem : lista) {
+                   if (elem.getNombre().equals(nombre)) {
+                      if (elem.getRacha_Maxima()< racha_Maxima) {
+                          System.out.println("¡NUEVA RACHA MÁXIMA! ("+racha_Maxima+"victorias)");
+                      }
+                       
+                   }
+                }
               
 
-            }else if (entrada.equals("D")) {
+            }else if (entrada.toUpperCase().equals("D")) {
                 derrotas++;
                 racha = 0;
             
 
-            } else if (entrada.equals("E")) {
+            } else if (entrada.toUpperCase().equals("E")) {
                 empates++;
                 racha= 0;
      
 
-            } else if (entrada.equals("FIN")) {
+            } else if (entrada.toUpperCase().equals("FIN")) {
                 break;
             }else {
                System.out.println("Escriba una opcion valida V, D, E o FIN");
+               contadorpartida--;
             }
             
         }
 
-        float winrate = (victorias / contadorpartida) * 100;
-        float lostrate = (derrotas / contadorpartida) * 100;
-        float drawrate = (empates / contadorpartida) * 100;
+    
+        Jugador Jugador = new Jugador(derrotas, empates, nombre, racha, racha_Maxima, victorias);
+        lista.add(Jugador);
 
+
+        double winrate = (((double)victorias / contadorpartida) * 100);
+        double lostrate = ((double)derrotas / contadorpartida) * 100;
+        double drawrate = ((double)empates / contadorpartida) * 100;
+
+        MostrarInfo(nombre, contadorpartida, victorias, winrate, derrotas, lostrate, empates, drawrate, racha_Maxima);
+        
+        try (ObjectOutputStream ou = new ObjectOutputStream(new FileOutputStream("Ejer6/src/Datos.dat"))) {
+
+            ou.writeObject(Jugador);
+
+        } catch (Exception e) {
+            System.out.println("Error" + e);
+        }
+        }
+
+        
+
+
+    private static void MostrarInfo(String nombre, int contadorpartida, int victorias, double winrate, int derrotas, double lostrate, int empates, double drawrate, int racha_Maxima) {
         System.out.println("\n=== RESUMEN DE SESIÓN ===");
         System.out.println("Jugador: "+nombre);
         System.out.println("\nTotal de partidas jugadas: " + (contadorpartida));
         System.out.println("Victorias : "+ victorias+" ("+winrate+"%)");
         System.out.println("Derrotas : "+ derrotas+" ("+lostrate+"%)");
         System.out.println("Empates : "+ empates+" ("+drawrate+"%)");
-        System.out.println("\nRacha máxima: "+racha_Maxima+" victorias");
+        System.out.println("\nRacha máxima: "+racha_Maxima+" victorias\n");
 
-
+        if (winrate >=90) {
+            System.out.println("Clasificación: "+Clasificacion.LEYENDA);
+        }else if (winrate >=70) {
+            System.out.println("Clasificación: "+Clasificacion.PRO);
+        }else if (winrate >=50) {
+            System.out.println("Clasificación: "+Clasificacion.COMPETENTE);
+        }else if (winrate >=30) {
+            System.out.println("Clasificación: "+Clasificacion.EQUILIBRADO);
+        }else if (winrate <29) {
+            System.out.println("Clasificación: "+Clasificacion.APRENDIZ);
+        }
     }
-}
+
+   
+    }
