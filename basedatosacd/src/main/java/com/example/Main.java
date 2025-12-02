4517b05298306dc5ae91e07ee0d6c3e5c93363f7
AleBLorenzo <alejandrobl0819@gmail.com>
2025-12-02 10:40:17 +0000

    yes

---
+++ basedatosacd/src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+       
+        String URL = "jdbc:mysql://localhost:3306";
+
+        Connection con = DriverManager.getConnection(URL);
+
+        DatabaseMetaData MeTa = con.getMetaData();
+
+        System.out.println(MeTa.getDatabaseProductName());
+        
+    }
+}
